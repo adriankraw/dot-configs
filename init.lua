3f6e41206f17f06c2a41919bbd43418ea247cc72
adriankraw <adriankraw1@googlemail.com>
2023-11-02 09:08:34 +0100

    fixed nil Line:529

--- init.lua
+++ init.lua
@@ -526,7 +526,7 @@ mason_lspconfig.setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {
       cmd = (servers[server_name] or {}).command,
-      arguments = servers[server_name].arguments,
+      arguments = (servers[server_name] or {}).arguments,
       capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
